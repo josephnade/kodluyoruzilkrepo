d8b62eacc8caafac56a990c138e73ce1ea1c5744
Yusuf Akın <yusuf.akn2017@hotmail.com>
2023-11-16 20:18:16 +0300

    Add files via upload

---
+++ nodejs/modular_circle/main.js
@@ -0,0 +1,4 @@
+const { circleArea, circleCircumference } = require('./calculating_circle.js');
+
+console.log(`Daire Alanı: ${circleArea(5)}`);
+console.log(`Daire Çevresi: ${circleCircumference(5)}`);
